9dd7cfa3bcf2cd3c13acc3235e125e6047ae02d8
tptacles <kenxu.xu@gmail.com>
2023-03-05 23:51:29 -0500

    update

--- SimpleSwap.sol
+++ SimpleSwap.sol
@@ -1,10 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.7;
 
-//import '@uniswap/v2-periphery/contracts/libraries/UniswapV2Library.sol';
 import '@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol';
 import '@uniswap/v2-periphery/contracts/interfaces/IERC20.sol';
-//import '@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol';
 
 error TransferFormFailed();
 error AproveFailed();

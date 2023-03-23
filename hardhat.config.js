4b4f0cf2899d76727639d6f98b55dbc0e060457c
tptacles <kenxu.xu@gmail.com>
2023-03-23 02:54:56 -0400

    added hardhard package and goerli forking

---
+++ hardhat.config.js
@@ -0,0 +1,13 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.18",
+  networks: {
+    hardhat: {
+      forking: {
+        url: process.env.GOERLI_RPC_URL,
+      },
+    },
+  },
+};

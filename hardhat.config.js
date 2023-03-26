35f67a3078967cf7d6032622eff7d44375ae06a9
tptacles <kenxu.xu@gmail.com>
2023-03-26 06:33:07 -0400

    added two scripts to fork to mainnet and the goerli test net

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,13 +1,7 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
-  networks: {
-    hardhat: {
-      forking: {
-        url: process.env.GOERLI_RPC_URL,
-      },
-    },
-  },
+  defaultNetwork: "hardhat",
 };
